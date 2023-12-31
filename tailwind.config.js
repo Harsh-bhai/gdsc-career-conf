d5a05e7b247e5ab2f8f95b5304767c4e7d720ae2
Vaibhav Shukla <shukla.vaibhav1077@gmail.com>
2023-12-31 13:36:17 +0530

    added font and colors | removed other components | start work

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,12 +4,35 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
- 
+
     // Or if using `src` directory:
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        md: { max: "1090px" },
+      },
+      fontFamily: {
+        primary: ["var(--primary-font)"],
+        secondary: ["var(--secondary-font)"],
+      },
+      fontWeight: {
+        normal: "var(--font-normal)",
+        medium: "var(--font-medium)",
+      },
+      colors: {
+        primary: "var(--primary-color)",
+        background: "var(--background-color)",
+        dark: "var(--dark-color)",
+        light: "var(--light-color)",
+      },
+      zIndex: {
+        tooltip: "var(--z-tooltip)",
+        fixed: "var(--z-fixed)",
+        modal: "var(--z-modal)",
+      },
+    },
   },
   plugins: [],
-}
+};

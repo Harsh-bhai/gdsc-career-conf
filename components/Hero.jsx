0444101c901e64c803cb4fe75882ebc3c800fa37
Vaibhav Shukla <shukla.vaibhav1077@gmail.com>
2024-01-03 22:23:35 +0530

    small changes

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -18,7 +18,7 @@ const Hero = () => {
         </div>
 
         {/* Main Content */}
-        <div className="main-text absolute md:top-1/4 md:left-1/2 transform md:-translate-x-1/2 md:-translate-y-1/2 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/4 font-secondary md:text-[15em] text-7xl overflow-hidden">
+        <div className="main-text absolute md:top-1/4 md:left-1/2 transform md:-translate-x-1/2 md:-translate-y-1/2 top-1/3 left-1/2 -translate-x-1/2 -translate-y-1/3 font-secondary md:text-[15em] text-7xl overflow-hidden">
           <div className="flex justify-center items-center h-full mx-auto whitespace-nowrap">
             <h1 className="md:text-center flex md:flex-row flex-col md:gap-20">
               <span>CAREER</span>
@@ -32,16 +32,20 @@ const Hero = () => {
         </div>
 
         {/* Location and Venue */}
-        <div className="p-8 relative md:-top-[11em] top-[20em] left-1/2 transform -translate-x-1/2 flex flex-col gap-8 md:text-3xl text-xl">
+        <div className="p-8 relative md:-top-[11em] top-[15em] left-1/2 transform -translate-x-1/2 flex flex-col gap-8 md:text-3xl text-xl">
           <div className="quote text-center">
             A <span className="text-primary">one-day conference</span> to infuse
             fresh perspectives.
           </div>
           <div className="info flex flex-row gap-4 items-center justify-center font-secondary md:text-4xl">
-            <FaCalendarAlt style={{ color: "var(--primary-color)" }} />{" "}
-            04.02.2024 &nbsp;
-            <FaMapLocationDot style={{ color: "var(--primary-color)" }} /> BIT
-            Durg
+            <div className="date flex flex-row gap-2">
+              <FaCalendarAlt style={{ color: "var(--primary-color)" }} />
+              <span>04.02.2024</span>
+            </div>
+            <div className="loc flex flex-row gap-2">
+              <FaMapLocationDot style={{ color: "var(--primary-color)" }} />
+              <span>BIT Durg</span>
+            </div>
           </div>
         </div>
 

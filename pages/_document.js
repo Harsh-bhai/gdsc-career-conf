1f1a86a3dc931f31e002bdac5c7d7de99cb6d109
Vaibhav Shukla <shukla.vaibhav1077@gmail.com>
2024-01-19 10:11:31 +0530

    updated metadata

--- pages/_document.js
+++ pages/_document.js
@@ -4,17 +4,42 @@ export default function Document() {
   return (
     <Html lang="en" className="scroll-smooth">
       <Head>
-        {/* Add your custom meta tags here */}
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <title>GSDC Career Conf</title>
         <meta
           name="google-site-verification"
           content="bsRPIU_p6Ax7n_Qu44XpRptq2uAASJ5YLxn2OrDe2eM"
         />
+        <title>GSDC Career Conf</title>
+        <meta name="title" content="GSDC Career Conf" />
         <meta
           name="description"
           content="GDSC CareerConf, a one-day conference hosted by Google Developers Student Clubs, BIT-D, is dedicated to broadening horizons within the tech industry for students. Our mission is to provide an understanding of diverse career opportunities in tech."
+        />
+
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://gdsccareerconf.netlify.app/" />
+        <meta property="og:title" content="GSDC Career Conf" />
+        <meta
+          property="og:description"
+          content="GDSC CareerConf, a one-day conference hosted by Google Developers Student Clubs, BIT-D, is dedicated to broadening horizons within the tech industry for students. Our mission is to provide an understanding of diverse career opportunities in tech."
+        />
+        <meta
+          property="og:image"
+          content="https://gdsccareerconf.netlify.app/logo3.png"
+        />
+
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta
+          property="twitter:url"
+          content="https://gdsccareerconf.netlify.app/"
+        />
+        <meta property="twitter:title" content="GSDC Career Conf" />
+        <meta
+          property="twitter:description"
+          content="GDSC CareerConf, a one-day conference hosted by Google Developers Student Clubs, BIT-D, is dedicated to broadening horizons within the tech industry for students. Our mission is to provide an understanding of diverse career opportunities in tech."
+        />
+        <meta
+          property="twitter:image"
+          content="https://gdsccareerconf.netlify.app/logo3.png"
         />
       </Head>{" "}
       <body>

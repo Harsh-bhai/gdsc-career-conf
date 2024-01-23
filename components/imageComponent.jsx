7ccad1d80b4884dc35caa25beee7bbe656765f09
Vaibhav Shukla <shukla.vaibhav1077@gmail.com>
2024-01-23 21:58:55 +0530

    basic team page created

--- components/imageComponent.jsx
+++ components/imageComponent.jsx
@@ -1,22 +1,43 @@
 /* eslint-disable @next/next/no-img-element */
 // components/ImageComponent.js
 
-const ImageComponent = ({ imgUrl, personName,Type }) => {
-    return (
-      <div className={`relative`}>
-     
-        <div className="absolute  top-[40%] -right-[20%] text-white text-center p-2 bg-opacity-50 ">
-          {personName.split(" ").map((item)=>{
-            return <p key={item} className="md:text-sm text-left text-xxs">{item}</p>
-          })}
-          <p className="md:text-xs  text-3xs border-red-500 text-left border-2 border-l-0 border-r-0 border-b-0">{Type}</p>
+const ImageComponent = ({ imgUrl, personName, Type, size }) => {
+  const imageSize =
+    size === "large"
+      ? "md:w-[18rem] w-[10rem] md:h-[18rem] h-[10rem]"
+      : "md:w-[14rem] w-[10rem] md:h-[14rem] w-[10rem]";
+
+  return (
+    <div className="relative">
+      <img src="/stars.png" alt="" className="absolute inset-0 -z-10" />
+      <img
+        src={imgUrl}
+        alt={personName}
+        className={`${imageSize} object-cover rounded-md`}
+      />
+      <div className="md:absolute md:top-[40%] md:-right-[20%]  text-white text-center p-2 bg-opacity-50">
+        <div className="flex md:flex-col items-start flex-row md:gap-0 gap-2">
+          {" "}
+          {personName.split(" ").map((item) => (
+            <p
+              key={item}
+              className={`${
+                size === "large"
+                  ? "md:text-lg text-sm "
+                  : "md:text-base text-sm"
+              }} text-left `}
+            >
+              {item}
+            </p>
+          ))}
         </div>
-        <img src="/stars.png" alt="" className="absolute inset-0 -z-10"/>
-        <img src={imgUrl} alt={personName} className="md:w-[16rem] md:h-[16rem] w-[8rem] h-[8rem] object-cover shadow-2xl rounded-md " />
-        </div>
-      
-    );
-  };
-  
-  export default ImageComponent;
-  
+
+        <p className="md:text-xs text-3xs border-red-500 text-left border-2 border-l-0 border-r-0 border-b-0">
+          {Type}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default ImageComponent;
--- pages/team/index.jsx
+++ pages/team/index.jsx
@@ -1,93 +1,163 @@
 import React from "react";
 import ImageComponent from "@/components/imageComponent";
 import Image from "next/image";
+import Sponsors from "@/components/Sponsors";
 const Team = () => {
-  const teamMembers = [
+  const organizer = [
     {
       imgUrl: "r8xjoe-removebg-preview 1.png",
-      personName: "Dhruv Advadkar",
-      Type: "Design Team",
+      personName: "Poorva Diwan",
+      Type: "Organizer",
+    },
+    {
+      imgUrl: "r8xjoe-removebg-preview 1.png",
+      personName: "Shashwat Gupta",
+      Type: "Organizer",
+    },
+    {
+      imgUrl: "r8xjoe-removebg-preview 1.png",
+      personName: "Dhruv Adavadkar",
+      Type: "Organizer",
     },
     {
       imgUrl: "r8xjoe-removebg-preview 1.png",
       personName: "Vaibhav Shukla",
-      Type: "Web Team",
+      Type: "Organizer",
     },
+  ];
+  const coreTeam = [
     {
       imgUrl: "r8xjoe-removebg-preview 1.png",
-      personName: "Member 2",
-      Type: "Marketing Team",
-    },
-    {
-      imgUrl: "r8xjoe-removebg-preview 1.png",
-      personName: "Member 2",
-      Type: "Marketing Team",
-    },
-    {
-      imgUrl: "r8xjoe-removebg-preview 1.png",
-      personName: "Member 2",
-      Type: "Marketing Team",
-    },
-    {
-      imgUrl: "r8xjoe-removebg-preview 1.png",
-      personName: "Dhruv Advadkar",
+      personName: "Shashank Yadav",
       Type: "Design Team",
     },
     {
       imgUrl: "r8xjoe-removebg-preview 1.png",
-      personName: "Member 2",
+      personName: "Shruti Pandey",
+      Type: "Web Team",
+    },
+    {
+      imgUrl: "r8xjoe-removebg-preview 1.png",
+      personName: "Aditi Sharma",
       Type: "Marketing Team",
     },
     {
       imgUrl: "r8xjoe-removebg-preview 1.png",
-      personName: "Member 2",
+      personName: "Harsh Dewangan",
+      Type: "Design Team",
+    },
+    {
+      imgUrl: "r8xjoe-removebg-preview 1.png",
+      personName: "Risabh Gupta",
       Type: "Marketing Team",
     },
     {
       imgUrl: "r8xjoe-removebg-preview 1.png",
-      personName: "Member 2",
+      personName: "Shreya Badgaiyan",
       Type: "Marketing Team",
     },
     {
       imgUrl: "r8xjoe-removebg-preview 1.png",
-      personName: "Member 2",
-      Type: "Marketing Team",
+      personName: "Prapti Sanghi",
+      Type: "Design Team",
+    },
+    {
+      imgUrl: "r8xjoe-removebg-preview 1.png",
+      personName: "Tanu Shree",
+      Type: "Design Team",
+    },
+    {
+      imgUrl: "r8xjoe-removebg-preview 1.png",
+      personName: "Priyansh Gupta",
+      Type: "Design Team",
+    },
+    {
+      imgUrl: "r8xjoe-removebg-preview 1.png",
+      personName: "Ansh Joseph",
+      Type: "Design Team",
+    },
+    {
+      imgUrl: "r8xjoe-removebg-preview 1.png",
+      personName: "Priyansh Gupta",
+      Type: "Design Team",
     },
   ];
 
-  const sectionNames=[ "Organizers", "Core Team", "Volunteers"]
+  const volunteers = [
+    {
+      imgUrl: "r8xjoe-removebg-preview 1.png",
+      personName: "Poorva Diwan",
+      Type: "Organizer",
+    },
+    {
+      imgUrl: "r8xjoe-removebg-preview 1.png",
+      personName: "Shashwat Gupta",
+      Type: "Organizer",
+    },
+    {
+      imgUrl: "r8xjoe-removebg-preview 1.png",
+      personName: "Dhruv Adavadkar",
+      Type: "Organizer",
+    },
+    {
+      imgUrl: "r8xjoe-removebg-preview 1.png",
+      personName: "Vaibhav Shukla",
+      Type: "Organizer",
+    },
+  ];
 
   return (
-    <div className="relative text-white  justify-center items-center  min-h-screen pt-20 ">
-      <h1 className="mb-20  text-4xl text-center px-8  font-semibold md:text-6xl ">
-        {" "}
+    <div className="relative text-white flex flex-col items-center min-h-screen md:pt-36 pt-24">
+      <h1 className="text-4xl text-center px-8  font-semibold md:text-6xl ">
         MEET OUR TEAM
       </h1>
-      { sectionNames.map((item)=>{
-        return(
-          <section key={item} className="pb-40" >
-        <h1 className=" text-primary text-3xl md:text-5xl text-center font-semibold md:font-extrabold">{item}</h1>
-        <div className=" flex flex-wrap justify-center  gap-20 mt-10">
-          {teamMembers.map((member, index) => (
+      <div className="mt-12">
+        <h1 className=" text-primary text-3xl md:text-5xl text-center font-semibold md:font-extrabold">
+          Organizers
+        </h1>
+        <div className="grid md:grid-cols-4 grid-cols-2 justify-center md:gap-24 gap-4 mt-10">
+          {organizer.map((item, index) => (
             <ImageComponent
               key={index}
-              imgUrl={member.imgUrl}
-              personName={member.personName}
-              Type={member.Type}
+              imgUrl={item.imgUrl}
+              personName={item.personName}
+              Type={item.Type}
+              size={"large"}
             />
           ))}
         </div>
-      </section>
-        )
-      })}
-        <Image
-        src={"/bg-asset.svg"}
-        height={700}
-        width={500}
-        alt=""
-        className="-z-30 absolute left-0 top-0 rotate-180"
-      />
-        <Image
+      </div>
+      <div className="md:mt-24 mt-12 md:w-[95%]">
+        <h1 className=" text-primary text-3xl md:text-5xl text-center font-semibold md:font-extrabold">
+          Core Team
+        </h1>
+        <div className="flex flex-wrap justify-center md:gap-20 gap-4 mt-10">
+          {coreTeam.map((item, index) => (
+            <ImageComponent
+              key={index}
+              imgUrl={item.imgUrl}
+              personName={item.personName}
+              Type={item.Type}
+            />
+          ))}
+        </div>
+      </div>
+      <div className="mt-24 mt-12 md:w-[95%]">
+        <h1 className=" text-primary text-3xl md:text-5xl text-center font-semibold md:font-extrabold">
+          Volunteers
+        </h1>
+        <div className="flex flex-wrap justify-center md:gap-20 gap-4 mt-10">
+          {volunteers.map((item, index) => (
+            <ImageComponent
+              key={index}
+              imgUrl={item.imgUrl}
+              personName={item.personName}
+              Type={item.Type}
+            />
+          ))}
+        </div>
+      </div>
+      <Image
         src={"/bg-asset.svg"}
         height={700}
         width={500}

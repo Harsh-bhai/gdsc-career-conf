0281e0be146d72d6f222319315fde7320c4995b8
Harsh Dewangan <harshdewangan2019@gmail.com>
2024-02-05 23:07:41 +0530

    tickets expired

--- pages/register/index.jsx
+++ pages/register/index.jsx
@@ -3,14 +3,15 @@ import React from "react";
 const page = () => {
   return (
     <div className="h-screen w-screen text-white">
-      <div className="pt-20 h-full">
-        <iframe
+      <div className="pt-20 h-full flex justify-center items-center">
+      <span className="font-secondary md:text-[3.5vw] text-4xl md:p-0 md:text-left text-center">Tickets Expired</span>
+        {/* <iframe
           src="https://konfhub.com/widget/gdsc-careerconf?desc=true"
           id="konfhub-widget"
           title="Register for GDSC CareerConf"
           width="100%"
           height="100%"
-        ></iframe>
+        ></iframe> */}
       </div>
     </div>
   );
